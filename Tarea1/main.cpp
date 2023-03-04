52f1d89d630f93705b9776a0cdfd2de0e5ea1ec9
arty4325 <arturo.acuna.duran@gmail.com>
2023-03-04 08:31:03 -0600

    Se programa la lista simplemente enlazada

--- Tarea1/main.cpp
+++ Tarea1/main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+class Node{
+public:
+    int value;
+    Node* next;
+
+    Node (int value){
+        this->value = value;
+        this->next = nullptr;
+    }
+};
+
+class ListaSimple{
+private:
+    Node* head;
+
+public:
+    ListaSimple(){
+        this->head = nullptr;
+    }
+    Node* getHead(){
+        return this->head;
+    }
+    void setHead(Node* newHead){
+        this->head = newHead;
+    }
+    void insertHead(int value){
+        Node* newNode = new Node(value);
+        newNode -> next = this -> head;
+        this -> head = newNode;
+    }
+
+    void printList(){
+        Node* current = this->head;
+        while(current != nullptr){
+            cout << current->value << endl;
+            current = current -> next;
+        }
+
+    }
+
+};
+
+int main(){
+    ListaSimple lista;
+    lista.insertHead(1);
+    lista.insertHead(2);
+    lista.insertHead(3);
+    lista.printList();
 }
+
+
+
